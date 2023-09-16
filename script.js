c531109449f94999c7162ee5d6f21a876d9427e0
mararo-ops <danielmararo1@gmail.com>
2023-09-16 15:50:10 +0300

    project

---
+++ script.js
@@ -0,0 +1,5 @@
+function hoverButton() {
+    let btn = document.getElementById("submitBtn");
+    btn.innerText = "Thanks for your remark!";
+    btn.disabled = true;  // This will disable the button
+}
